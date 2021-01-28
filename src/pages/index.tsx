e4d9f0dd0bd9ed8ed2d207c22f745324897676d9
Dawid Banachowski <bajkull@gmail.com>
2021-01-28 22:55:31 +0100

    landing adventure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Layout from "../layout/Layout";
 import chessboard from "../data/images/chessboard.png";
 import "../scss/landing.scss";
+import Pawn from "../data/svg/pawn.svg";
+import Queen from "../data/svg/queen.svg";
 
 export default function index() {
   return (
@@ -9,7 +11,7 @@ export default function index() {
       <div className="landing">
         <div className="content">
           <div className="mainLanding">
-            <header className="landingHeader">
+            <header className="landingHeader card">
               <h1 className="landingTitle">
                 Chess is the struggle against the error.
               </h1>
@@ -19,6 +21,21 @@ export default function index() {
               <button className="mainButton">Read more</button>
             </header>
             <img src={chessboard} alt="" className="mainIllustration" />
+          </div>
+          <div className="adventure">
+            <h1 className="adventureTitle">Start you chess adventure now!</h1>
+            <div className="cards">
+              <div className="card">
+                <h2>I'm a complete begginer</h2>
+                <Pawn className="adventureImg" />
+                <button className="mainButton">See more</button>
+              </div>
+              <div className="card">
+                <h2>I already know the basics</h2>
+                <Queen className="adventureImg" />
+                <button className="mainButton">See more</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
