ba341c47f871322fd02f80ba5b166d39e0717a27
Dawid Banachowski <bajkull@gmail.com>
2021-01-30 17:25:10 +0100

    navbar responsiveness and style

--- src/navbar/Navbar.tsx
+++ src/navbar/Navbar.tsx
@@ -1,16 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "gatsby";
 import Logo from "../data/svg/logo.svg";
+import Burger from "../data/svg/burger.svg";
 import "../scss/navbar.scss";
 
 export default function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+  const [hide, setHide] = useState(false);
+  const [scaledDown, setScaledDown] = useState(false);
+  const [scrollPos, setScrollPos] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const newPos = window.scrollY;
+      const width = window.innerWidth;
+
+      console.log(newPos, scrollPos);
+
+      if (newPos >= scrollPos && width < 800) {
+        setHide(true);
+        setScaledDown(true);
+        setShowMenu(false);
+      } else if (newPos >= scrollPos) {
+        setScaledDown(true);
+        setShowMenu(false);
+      } else {
+        setHide(false);
+        if (newPos <= 100) setScaledDown(false);
+      }
+      setScrollPos(newPos);
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [scrollPos]);
+
+  if (hide) return null;
   return (
-    <div className="navbar">
+    <div className={scaledDown ? "navbarMini" : "navbar"}>
       <div className="content">
-        <ul>
-          <Link to="/">
-            <Logo className="logo" />
-          </Link>
+        <Link to="/" className="logoContainer">
+          <Logo className="logo" />
+        </Link>
+        <Burger className="burger" onClick={() => setShowMenu(!showMenu)} />
+        <ul className={showMenu ? "shown" : "hidden"}>
           <Link to="/pieces">
             <li>Pieces</li>
           </Link>
